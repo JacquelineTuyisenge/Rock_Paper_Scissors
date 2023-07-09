1610378763e350821e53a841ccb6d106a6856b61
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-07-09 22:08:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choice.length);
-    return choice[randomIndex];
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase(); // Convert player's input to lowercase
+  computerSelection = computerSelection.toLowerCase(); // Convert computer's choice to lowercase
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let round = 1; round <= 5; round++) {
+    const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
+    const computerSelection = getComputerChoice();
+
+    const result = playRound(playerSelection, computerSelection);
+    console.log("Round " + round + ": " + result);
+
+    if (result.includes("win")) {
+      playerScore++;
+    } else if (result.includes("lose")) {
+      computerScore++;
+    }
+  }
+
+  console.log("Final Score: You: " + playerScore + " | Computer: " + computerScore);
+  if (playerScore > computerScore) {
+    console.log("Congratulations! You win the game!");
+  } else if (playerScore < computerScore) {
+    console.log("Sorry, you lose the game.");
+  } else {
+    console.log("It's a tie game!");
+  }
+}
+
+game();
